1626dc622cfa33e118917085aa42ba9d95e242bd
Vitor Bionic <vitorbionic@hotmail.com>
2025-07-27 15:34:38 -0300

    Starting project

---
+++ src/test/java/com/vitorbionic/tasktrackercli/TaskTrackerCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorbionic.tasktrackercli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
